4aa0c9ca77088ac7921affb3c3b992f4f136dbcf
Student <s1091828@gm.pu.edu.tw>
2022-04-19 11:01:35 +0800

    v3: 判斷是否跟其他圖片碰撞

--- app/src/main/java/com/example/ghosttt/MainActivity.kt
+++ app/src/main/java/com/example/ghosttt/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ghosttt
 
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
@@ -26,6 +27,20 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         if (event?.action == MotionEvent.ACTION_MOVE){
             v?.x = event.rawX - v!!.width/2
             v?.y = event.rawY - v!!.height/2
+
+            var r1: Rect = Rect(v.x.toInt(), v.y.toInt(),
+                    v.x.toInt() + v.width, v.y.toInt() + v.height)
+            var r2: Rect = Rect(binding.img2.x.toInt(), binding.img2.y.toInt(),
+                    binding.img2.x.toInt() + binding.img2.width,
+                    binding.img2.y.toInt() + binding.img2.height)
+
+            if(r1.intersect(r2)) {
+                binding.txv.text = "碰撞"
+            }
+            else{
+                binding.txv.text = ""
+            }
+
         }
 
         return true
