681d03dcc6b1fa72ee5e0824a810bbfd91f47e6e
Student <s1091828@gm.pu.edu.tw>
2022-04-19 10:59:49 +0800

    v2: 刪除標題列 + 拖曳圖片

--- app/src/main/java/com/example/ghosttt/MainActivity.kt
+++ app/src/main/java/com/example/ghosttt/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.ghosttt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import com.example.ghosttt.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnTouchListener {
 
     lateinit var binding: ActivityMainBinding
 
@@ -15,5 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.txv.text = "拖曳及碰撞偵測"
+        binding.img1.setOnTouchListener(this)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        binding.txv.text = "觸控"
+
+        if (event?.action == MotionEvent.ACTION_MOVE){
+            v?.x = event.rawX - v!!.width/2
+            v?.y = event.rawY - v!!.height/2
+        }
+
+        return true
     }
 }
